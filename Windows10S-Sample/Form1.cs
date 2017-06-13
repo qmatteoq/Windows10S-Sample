9a15efa05ecd484000213f62ecad97ca4278765e
Matteo Pagani <info@qmatteoq.com>
2017-06-13 08:50:26 +0200

    Fixed source code formatting

--- Windows10S-Sample/Form1.cs
+++ Windows10S-Sample/Form1.cs
@@ -1,12 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Diagnostics;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Windows10S_Sample
@@ -18,17 +11,17 @@ namespace Windows10S_Sample
             InitializeComponent();
         }
 
-private void OpenCommandPrompt(object sender, EventArgs e)
-{
-    Process.Start("CMD.EXE");
-}
+        private void OpenCommandPrompt(object sender, EventArgs e)
+        {
+            Process.Start("CMD.EXE");
+        }
 
-private void OpenConsoleApp(object sender, EventArgs e)
-{
-    string location = System.Reflection.Assembly.GetExecutingAssembly().Location;
-    int index = location.LastIndexOf("\\");
-    string source = $"{location.Substring(0, index)}\\Windows10S-Console.exe";
-    Process.Start(source);
-}
+        private void OpenConsoleApp(object sender, EventArgs e)
+        {
+            string location = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            int index = location.LastIndexOf("\\");
+            string source = $"{location.Substring(0, index)}\\Windows10S-Console.exe";
+            Process.Start(source);
+        }
     }
 }
